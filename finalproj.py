87104249b13a9c568018d43bc4289aecc4dc59f6
SaulR678 <srluna0205@yahoo.com>
2025-05-21 15:00:10 -0400

    added picture function, submit button. and started show error function...

--- finalproj.py
+++ finalproj.py
@@ -11,10 +11,26 @@ def main(page: ft.Page):
     year = ft.TextField(label="year",width=100)
     hour = ft.TextField(label="hour",width=100)
     minute = ft.TextField(label="minute",width=100)
-    
+
+    def show_error(e):
+        pass
+
+    def add_pic(e):
+        try:
+            y=int(year.value)
+            m=int(month.value)
+            d=int(day.value)
+            h=int(hour.value)
+            min=int(minute.value)
+        except ValueError:
+            show_error("All fields must contain valid number.")
+    submit= ft.Elevatedbutton("Submit", width=100, on_click=add_pic)
+
+
     page.add(ft.Row([star,text,star], alignment = ft.MainAxisAlignment.CENTER),
         ft.Divider(),
         ft.Row([day,month,year,hour,minute],
-        alignment = ft.MainAxisAlignment.CENTER))
+        alignment = ft.MainAxisAlignment.CENTER),
+        ft.Row([submit],aligment=ft.MainAxisAligment.CENTER))
 
 ft.app(target = main, assets_dir="assets")
